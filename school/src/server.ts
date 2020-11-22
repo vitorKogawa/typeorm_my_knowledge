f7e49904343854abc6ec94e731256f1c47f57c26
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2020-11-22 17:27:12 -0300

    Adição do express (server.ts, routes.ts), alteração no arquivo de configuração do ORM, criação da migração para criação da tabela de classe

---
+++ school/src/server.ts
@@ -0,0 +1,15 @@
+import * as express from 'express'
+import routes from './routes'
+import './config/env'
+import './database/index'
+
+const server = express()
+server.use(express.json())
+server.use(routes)
+const port = process.env.PORT || 3333
+
+server.use('*', (request, response) => {
+    response.send('<h1>Status: Online!</h1>')
+})
+
+server.listen(port, () => console.log('Servidor rodando na porta: ', port))
